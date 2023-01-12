6faca6235cf114ca23ddb179a7f88ccf03f05385
Anthony Victor <thonnyvrc@gmail.com>
2023-01-12 11:46:14 -0300

    feat: menu flavours included

--- src/pages/cardapio/index.tsx
+++ src/pages/cardapio/index.tsx
@@ -1,12 +1,50 @@
-import { GetStaticProps, NextPage } from "next";
+import { GetServerSideProps, GetStaticProps, NextPage } from "next";
 import Image from "next/image";
 import { PecaJaButton } from "../../components/pecaja";
 import { CardapioStyle } from "../../styles/pages/cardapio/styles";
 import PizzaImage from "../../assets/pages/home/pizza_1.png";
 import { useNavigation } from "../../context/navigationContext";
+import tamanhos from "../../data/tamanhos.json";
+import sabores from "../../data/sabores.json";
+import { useEffect } from "react";
 
-const Cardapio: NextPage = () => {
-  return <CardapioStyle></CardapioStyle>;
+const Cardapio: NextPage<{ left: []; right: [] }> = ({ left, right }) => {
+  const getContent = (g) => (
+    <div className="grupo">
+      <h2 className="grupo-nome">{g.nome}</h2>
+      <div className="grupo-sabores">
+        {g.sabores.map((s) => (
+          <div className="sabor">
+            <h5 className="sabor-nome">{s.nome}</h5>
+            <p className="sabor-ingredientes">{s.ingredientes.join(", ")}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  return (
+    <CardapioStyle>
+      <aside className="left">{left.map((g) => getContent(g))}</aside>
+      <aside className="right">{right.map((g) => getContent(g))}</aside>
+    </CardapioStyle>
+  );
 };
 
 export default Cardapio;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const left = [];
+  const right = [];
+
+  sabores.grupos.forEach((g) => {
+    sabores.grupos.indexOf(g) % 2 === 0 ? left.push(g) : right.push(g);
+  });
+
+  return {
+    props: {
+      left,
+      right,
+    },
+  };
+};
--- src/styles/pages/cardapio/styles.tsx
+++ src/styles/pages/cardapio/styles.tsx
@@ -5,5 +5,72 @@ import tabletShape from "../../../assets/pages/home/home-shape-center-tablet.svg
 import desktopShape from "../../../assets/pages/home/home-shape-center-desktop.svg";
 import { sizes } from "../../sizes";
 import { animations } from "../../animations";
+import { colors } from "../../colors";
 
-export const CardapioStyle = styled.main``;
+export const CardapioStyle = styled.main`
+  color: #fff;
+  height: 100%;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 5rem 0.3rem;
+
+  .left {
+    position: relative;
+  }
+  .left:after {
+    content: "";
+    background-color: ${colors.elements};
+    width: 2px;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: calc(100% - 4px);
+  }
+
+  .grupo {
+    display: flex;
+    flex-direction: column;
+    margin: 1px;
+    .grupo-nome {
+      align-self: center;
+      font-size: 2em;
+    }
+    .grupo-sabores {
+      display: flex;
+      flex-direction: column;
+
+      /* justify-content: center; */
+
+      .sabor {
+        margin: 0.2rem;
+        margin: 0.2rem 100px;
+        .sabor-nome {
+          font-size: 1.2rem;
+        }
+        .sabor-ingredientes {
+        }
+      }
+    }
+  }
+
+  @media ${breakpointsMQ.tabletUp} {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: auto;
+    grid-auto-flow: row dense;
+    /* display: inline-flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      */
+    aside {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+
+      .grupo {
+        grid-row: span auto;
+      }
+    }
+  }
+`;
