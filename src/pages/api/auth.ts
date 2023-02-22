52983128cab08160fe74e99e8a5d29fd636dfdf8
Anthony Victor <thonnyvrc@gmail.com>
2023-02-22 03:23:32 -0300

    added auth route

---
+++ src/pages/api/auth.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest as Request, NextApiResponse as Response } from "next";
+import { env } from "@config/env";
+import NextCors from "nextjs-cors";
+
+export default async function handler(req: Request, res: Response) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  if (req.method === "GET") {
+    try {
+      let { pw } = req.body;
+      if (!pw) throw new Error("Invalid password");
+      pw = Buffer.from(pw).toString("ascii");
+      if (pw === env.configPassword) {
+        res.status(200).json({});
+      }
+    } catch (e: any) {
+      console.error(e["message"]);
+      res.status(500).send({ message: "Invalid password" });
+    }
+  }
+}
