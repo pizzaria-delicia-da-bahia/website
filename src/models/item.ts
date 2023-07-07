1d1f40c353e77a6343bce7acfb7c5e987875d01c
Anthony Victor <thonnyvrc@gmail.com>
2023-07-07 01:34:03 -0300

    feat: added pedido

--- src/models/item.ts
+++ src/models/item.ts
@@ -3,7 +3,7 @@ import { IPizzaSabor, IPizzaTamanho } from "./pizza";
 export interface IItem {
   id?: string;
   observacao?: string;
-  tipo: "PIZZA" | "BEBIDA" | "OUTRO";
+  tipo?: "PIZZA" | "BEBIDA" | "OUTRO";
   valor: number;
 }
 export interface IPizza extends IItem {
