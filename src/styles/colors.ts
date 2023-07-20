6adfb0bea9f668a4d9479eefe0ccd99ae360c6f4
Anthony Victor <thonnyvrc@gmail.com>
2023-07-19 22:32:38 -0300

    barbie colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,12 @@
 export const colors = {
-  elements: "#FFBE18",
-  backgroundLight: "#c90202",
-  background: "#8D0000",
-  backgroundDark: "#5e0101",
+  elements: "#fff",
+  backgroundLight: "#f768dd",
+  background: "#940179",
+  backgroundDark: "#eb34c9",
 };
+// export const colors = {
+//   elements: "#FFBE18",
+//   backgroundLight: "#c90202",
+//   background: "#8D0000",
+//   backgroundDark: "#5e0101",
+// };
