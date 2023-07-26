5778331c01a65214ddf82491e47faad7bdbcb267
Anthony Victor <thonnyvrc@gmail.com>
2023-07-26 20:12:56 -0300

    reverted colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,13 +1,6 @@
 export const colors = {
-<<<<<<< HEAD
-  elements: "#ffb3f1",
-  backgroundLight: "#f768dd",
-  background: "#bf029c",
-  backgroundDark: "#eb34c9",
-=======
   elements: "#FFBE18",
   backgroundLight: "#c90202",
   background: "#8D0000",
   backgroundDark: "#5e0101",
->>>>>>> parent of 6adfb0b (barbie colors)
 };
