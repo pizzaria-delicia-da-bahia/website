4f1edff35217c9b53923f1cbbcac75b1ba1d4030
Anthony Victor <thonnyvrc@gmail.com>
2023-12-08 18:02:23 -0300

    promocao off

--- src/pages/pedido/index.tsx
+++ src/pages/pedido/index.tsx
@@ -31,11 +31,11 @@ const Pedido: NextPage = ({
       route: "pedido/pizza/tamanho",
       image: "/images/pedido-pizza.svg",
     },
-    {
-      name: "",
-      route: "pedido/promocao-relampago",
-      image: "/images/promocao.svg",
-    },
+    // {
+    //   name: "",
+    //   route: "pedido/promocao-relampago",
+    //   image: "/images/promocao.svg",
+    // },
     {
       name: "BEBIDAS",
       route: "pedido/bebida",
