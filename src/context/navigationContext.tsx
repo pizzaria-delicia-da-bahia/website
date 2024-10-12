0039a3fae8cfefa107d3e7ebd7edb6a7e8d0639a
thonnykjjjkk <thonnyvrc@gmail.com>
2024-10-12 06:03:17 -0300

    added kids promo

--- src/context/navigationContext.tsx
+++ src/context/navigationContext.tsx
@@ -22,15 +22,14 @@ const NavigationContext = createContext<{
 
 const NavigationProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [menuOpen, setMenuOpen] = useState<Boolean>(false);
-  const { getDuasRefri60, promosCarregadas } = usePromo();
+  const { getKids, getDuasRefri60, promosCarregadas } = usePromo();
   const [modalPromo, setModalPromo] = useState(<></>);
   const router = useRouter();
 
   useEffect(() => {
     if (!promosCarregadas) return;
-    console.log(router.asPath, router.pathname);
     setModalPromo(
-      getDuasRefri60() && router.pathname.startsWith("/home") ? (
+      getDuasRefri60() && !getKids() && router.pathname.startsWith("/home") ? (
         <ModalPromo2
           url="/pedido/promocao-duas"
           image="/images/promo-duas-refri-60-modal.png"
