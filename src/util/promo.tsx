19ced2445f989a3d6266b07039a730ddbf698fef
thonnykjjjkk <thonnyvrc@gmail.com>
2024-10-20 18:19:12 -0300

    taxa gratis

--- src/util/promo.tsx
+++ src/util/promo.tsx
@@ -1,7 +1,7 @@
 import { IPedido } from "@models/order";
 
 export const taxaGratisAteTalHoras = (order: IPedido) => {
-  const dataPromo = new Date("2024-10-18 20:05:00");
+  const dataPromo = new Date("2024-10-20 20:05:00");
 
   const now = new Date();
 
