082f5068b084f3e7e44b5f320c4b8284bb36c1a3
thonnykjjjkk <thonnyvrc@gmail.com>
2025-02-11 15:42:36 -0300

    added a bunch of things

--- src/pages/pedido/pagamento/Context.tsx
+++ src/pages/pedido/pagamento/Context.tsx
@@ -2,7 +2,7 @@ import { SetState } from "@config/react";
 import { useMyOrder } from "@context/myOrderContext";
 import { usePayment } from "@context/paymentContext";
 import { IPagamento } from "@models/order";
-import { formatCurrency, formatTipoPagamento } from "@util/format";
+import { formatCurrency } from "@util/format"; //formatTipoPagamento
 import { calcularTaxaMaquininha, subtrairTaxaMaquininha } from "@util/math";
 import { sleep } from "@util/misc";
 import { useRouter } from "next/router";
@@ -13,140 +13,137 @@ import { v4 as uuidv4 } from "uuid";
 interface IPagSemId extends Omit<IPagamento, "id"> {}
 
 interface IPagamentoPageContext {
-  pagamentos: IPagamento[];
-  setPagamentos: SetState<IPagamento[]>;
-  pagamentoAtual: IPagSemId | undefined;
-  setPagamentoAtual: SetState<IPagSemId | undefined>;
-
-  addPagamento: (pagamento?: IPagSemId) => void;
-
-  modal_change: boolean;
-  setModal_change: SetState<boolean>;
-  modal_tax: boolean;
-  setModal_tax: SetState<boolean>;
-  modal_value: boolean;
-  setModal_value: SetState<boolean>;
-  continuar: () => void;
-
-  valorPago: number;
-  valorPagoMenosTaxa: number;
-  valorPagoCartao: number;
-  valorPagoOutros: number;
+  // pagamentos: IPagamento[];
+  // setPagamentos: SetState<IPagamento[]>;
+  // pagamentoAtual: IPagSemId | undefined;
+  // setPagamentoAtual: SetState<IPagSemId | undefined>;
+  // addPagamento: (pagamento?: IPagSemId) => void;
+  // modal_change: boolean;
+  // setModal_change: SetState<boolean>;
+  // modal_tax: boolean;
+  // setModal_tax: SetState<boolean>;
+  // modal_value: boolean;
+  // setModal_value: SetState<boolean>;
+  // continuar: () => void;
+  // valorPago: number;
+  // valorPagoMenosTaxa: number;
+  // valorPagoCartao: number;
+  // valorPagoOutros: number;
 }
 const PagamentoPageContext = createContext<IPagamentoPageContext>(
   {} as IPagamentoPageContext
 );
 
 export const PagamentoPageProvider = ({ children }) => {
-  const [pagamentos, setPagamentos] = useState<IPagamento[]>([]);
-  const [pagamentoAtual, setPagamentoAtual] = useState<IPagSemId | undefined>();
+  // const [pagamentos, setPagamentos] = useState<IPagamento[]>([]);
+  // const [pagamentoAtual, setPagamentoAtual] = useState<IPagSemId | undefined>();
 
-  const [modal_change, setModal_change] = useState(false);
-  const [modal_tax, setModal_tax] = useState(false);
-  const [modal_value, setModal_value] = useState(false);
-  const { myOrder, addPaymentToOrder } = useMyOrder();
-  const { totalPedido, valorItensTaxados, taxaMaquininha } = usePayment();
+  // const [modal_change, setModal_change] = useState(false);
+  // const [modal_tax, setModal_tax] = useState(false);
+  // const [modal_value, setModal_value] = useState(false);
+  // const { myOrder, addPaymentToOrder } = useMyOrder();
+  // const { totalPedido, valorItensTaxados, taxaMaquininha } = usePayment();
 
-  const addPagamento = (pagamento?: IPagSemId) => {
-    const _pag =
-      pagamento.tipo && pagamento.valor
-        ? pagamento
-        : pagamentoAtual.tipo && pagamentoAtual.valor
-        ? pagamentoAtual
-        : undefined;
-    if (_pag.tipo && _pag.valor) {
-      setPagamentos((prev) => [
-        ...prev,
-        { ...pagamentoAtual, ...(pagamento ?? {}), id: uuidv4() },
-      ]);
+  // const addPagamento = (pagamento?: IPagSemId) => {
+  //   const _pag =
+  //     pagamento.tipo && pagamento.valor
+  //       ? pagamento
+  //       : pagamentoAtual.tipo && pagamentoAtual.valor
+  //       ? pagamentoAtual
+  //       : undefined;
+  //   if (_pag.tipo && _pag.valor) {
+  //     setPagamentos((prev) => [
+  //       ...prev,
+  //       { ...pagamentoAtual, ...(pagamento ?? {}), id: uuidv4() },
+  //     ]);
 
-      (async () => {
-        if (totalPedido - (valorPago + _pag.valor)) {
-          toast.success(
-            `${formatCurrency(_pag.valor)} ${formatTipoPagamento(
-              _pag.tipo
-            )} adicionados ao pagamento!`
-          );
-          await sleep(2000);
-          toast.success(
-            `Adicione o restante (${formatCurrency(
-              totalPedido - (valorPago + _pag.valor)
-            )}) para continuar`
-          );
-        }
-      })();
-    }
-  };
+  //     (async () => {
+  //       if (totalPedido - (valorPago + _pag.valor)) {
+  //         toast.success(
+  //           `${formatCurrency(_pag.valor)} ${formatTipoPagamento(
+  //             _pag.tipo
+  //           )} adicionados ao pagamento!`
+  //         );
+  //         await sleep(2000);
+  //         toast.success(
+  //           `Adicione o restante (${formatCurrency(
+  //             totalPedido - (valorPago + _pag.valor)
+  //           )}) para continuar`
+  //         );
+  //       }
+  //     })();
+  //   }
+  // };
 
-  const somar = (lista: { valor: number }[]) =>
-    lista.reduce((acc, curr) => acc + curr.valor, 0);
-  const valorPagoCartao = somar(pagamentos.filter((x) => x.tipo === "cartao"));
-  const valorPagoOutros = somar(pagamentos.filter((x) => x.tipo !== "cartao"));
-  const valorPago = somar(pagamentos);
+  // const somar = (lista: { valor: number }[]) =>
+  //   lista.reduce((acc, curr) => acc + curr.valor, 0);
+  // const valorPagoCartao = somar(pagamentos.filter((x) => x.tipo === "cartao"));
+  // const valorPagoOutros = somar(pagamentos.filter((x) => x.tipo !== "cartao"));
+  // const valorPago = somar(pagamentos);
 
-  const valorPagoMenosTaxa =
-    somar(pagamentos) -
-    (valorItensTaxados > 0
-      ? calcularTaxaMaquininha(
-          valorItensTaxados,
-          totalPedido -
-            (subtrairTaxaMaquininha(valorPagoCartao, taxaMaquininha) +
-              valorPagoOutros),
-          taxaMaquininha,
-          valorPagoOutros
-        )
-      : 0);
+  // const valorPagoMenosTaxa =
+  //   somar(pagamentos) -
+  //   (valorItensTaxados > 0
+  //     ? calcularTaxaMaquininha(
+  //         valorItensTaxados,
+  //         totalPedido -
+  //           (subtrairTaxaMaquininha(valorPagoCartao, taxaMaquininha) +
+  //             valorPagoOutros),
+  //         taxaMaquininha,
+  //         valorPagoOutros
+  //       )
+  //     : 0);
 
-  const router = useRouter();
+  // const router = useRouter();
 
-  const continuar = () => {
-    router.push(
-      `/pedido/confirmacao${
-        myOrder.tipo === "entrega"
-          ? `/${myOrder.cliente.endereco.bairroId}`
-          : ""
-      }`
-    );
-  };
+  // const continuar = () => {
+  //   router.push(
+  //     `/pedido/confirmacao${
+  //       myOrder.tipo === "entrega"
+  //         ? `/${myOrder.cliente.endereco.bairroId}`
+  //         : ""
+  //     }`
+  //   );
+  // };
 
-  useEffect(() => {
-    if (
-      valorPago ===
-      totalPedido +
-        calcularTaxaMaquininha(
-          valorItensTaxados,
-          valorPagoCartao,
-          taxaMaquininha,
-          valorPagoOutros
-        )
-    ) {
-      addPaymentToOrder(pagamentos);
-      continuar();
-    }
-  }, [valorPago]);
+  // useEffect(() => {
+  //   if (
+  //     valorPago ===
+  //     totalPedido +
+  //       calcularTaxaMaquininha(
+  //         valorItensTaxados,
+  //         valorPagoCartao,
+  //         taxaMaquininha,
+  //         valorPagoOutros
+  //       )
+  //   ) {
+  //     addPaymentToOrder(pagamentos);
+  //     continuar();
+  //   }
+  // }, [valorPago]);
 
   return (
     <PagamentoPageContext.Provider
-      value={{
-        pagamentos,
-        setPagamentos,
-        pagamentoAtual,
-        setPagamentoAtual,
-
-        addPagamento,
-
-        modal_change,
-        setModal_change,
-        modal_tax,
-        setModal_tax,
-        modal_value,
-        setModal_value,
-        continuar,
-        valorPago,
-        valorPagoMenosTaxa,
-        valorPagoCartao,
-        valorPagoOutros,
-      }}
+      value={
+        {
+          // pagamentos,
+          // setPagamentos,
+          // pagamentoAtual,
+          // setPagamentoAtual,
+          // addPagamento,
+          // modal_change,
+          // setModal_change,
+          // modal_tax,
+          // setModal_tax,
+          // modal_value,
+          // setModal_value,
+          // continuar,
+          // valorPago,
+          // valorPagoMenosTaxa,
+          // valorPagoCartao,
+          // valorPagoOutros,
+        }
+      }
     >
       {children}
     </PagamentoPageContext.Provider>
--- src/pages/pedido/pagamento/Tax.tsx
+++ src/pages/pedido/pagamento/Tax.tsx
@@ -7,72 +7,73 @@ import { useLayoutEffect } from "react";
 import { calcularTaxaMaquininha } from "@util/math";
 
 export const MachineTax = () => {
-  const { taxaMaquininha, valorItensTaxados, totalPedido, itensTaxados } =
-    usePayment();
-  const {
-    setModal_tax,
-    setModal_value,
-    pagamentoAtual,
-    setPagamentoAtual,
-    valorPago,
-    valorPagoOutros,
-  } = usePagamentoPage();
+  // const { taxaMaquininha, valorItensTaxados, totalPedido, itensTaxados } =
+  //   usePayment();
+  // const {
+  //   setModal_tax,
+  //   setModal_value,
+  //   pagamentoAtual,
+  //   setPagamentoAtual,
+  //   valorPago,
+  //   valorPagoOutros,
+  // } = usePagamentoPage();
 
-  const continuar = () => {
-    setModal_tax(false);
-    setModal_value(true);
-  };
+  // const continuar = () => {
+  //   setModal_tax(false);
+  //   setModal_value(true);
+  // };
 
-  useLayoutEffect(() => {
-    const _taxaProporcional =
-      pagamentoAtual.tipo === "cartao" && itensTaxados?.length
-        ? calcularTaxaMaquininha(
-            valorItensTaxados,
-            totalPedido - valorPago,
-            taxaMaquininha,
-            valorPagoOutros
-          )
-        : 0;
+  // useLayoutEffect(() => {
+  //   const _taxaProporcional =
+  //     pagamentoAtual.tipo === "cartao" && itensTaxados?.length
+  //       ? calcularTaxaMaquininha(
+  //           valorItensTaxados,
+  //           totalPedido - valorPago,
+  //           taxaMaquininha,
+  //           valorPagoOutros
+  //         )
+  //       : 0;
 
-    if (_taxaProporcional === 0) {
-      continuar();
-    }
-  }, []);
-  return (
-    <Modal
-      className="machine-tax-modal"
-      label={`${
-        pagamentoAtual.tipo === "cartao"
-          ? "Taxa da maquininha"
-          : "Taxa de processamento"
-      }`}
-      description={`Esse método de pagamento tem uma taxa de ${taxaMaquininha}%`}
-      subdescription="Deseja continuar?"
-      type={"custom"}
-      buttons={
-        <>
-          <ButtonPrimary
-            onClick={() => {
-              setPagamentoAtual(undefined);
-              setModal_tax(false);
-            }}
-          >
-            Voltar
-          </ButtonPrimary>
-          <ButtonSecondary onClick={continuar}>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              Continuar
-              <small style={{ color: "white" }}>*com a taxa</small>
-            </div>
-          </ButtonSecondary>
-        </>
-      }
-    >
-      <small>
-        Nossas promoções e combos exclusivas para pix ou dinheiro, podem também
-        ser pagas no cartão (crédito ou débito). Porém é acrescentada a taxa da
-        maquininha proporcional ao valor pago no cartão
-      </small>
-    </Modal>
-  );
+  //   if (_taxaProporcional === 0) {
+  //     continuar();
+  //   }
+  // }, []);
+  // return (
+  //   <Modal
+  //     className="machine-tax-modal"
+  //     label={`${
+  //       pagamentoAtual.tipo === "cartao"
+  //         ? "Taxa da maquininha"
+  //         : "Taxa de processamento"
+  //     }`}
+  //     description={`Esse método de pagamento tem uma taxa de ${taxaMaquininha}%`}
+  //     subdescription="Deseja continuar?"
+  //     type={"custom"}
+  //     buttons={
+  //       <>
+  //         <ButtonPrimary
+  //           onClick={() => {
+  //             setPagamentoAtual(undefined);
+  //             setModal_tax(false);
+  //           }}
+  //         >
+  //           Voltar
+  //         </ButtonPrimary>
+  //         <ButtonSecondary onClick={continuar}>
+  //           <div style={{ display: "flex", flexDirection: "column" }}>
+  //             Continuar
+  //             <small style={{ color: "white" }}>*com a taxa</small>
+  //           </div>
+  //         </ButtonSecondary>
+  //       </>
+  //     }
+  //   >
+  //     <small>
+  //       Nossas promoções e combos exclusivas para pix ou dinheiro, podem também
+  //       ser pagas no cartão (crédito ou débito). Porém é acrescentada a taxa da
+  //       maquininha proporcional ao valor pago no cartão
+  //     </small>
+  //   </Modal>
+  // );
+  return <></>;
 };
